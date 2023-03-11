d6972c4b62e2c905633f07ffe56d345706c8ff62
Riza Nur Maulana <rizanurmaulana2810@gmail.com>
2023-03-11 14:31:22 +0700

    update dark mode + fiture improvements

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = () => {
     const header = document.querySelector('header');
     const FixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
 
     if(window.pageYOffset > FixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -17,4 +22,33 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
-})
+});
+
+// Click outside the Hamburger Menu
+window.addEventListener('click', (e) => {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', () => {
+    if(darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// Move toggle position following Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
